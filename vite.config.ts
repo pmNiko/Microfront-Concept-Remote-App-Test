214407351a88e95030d62a1b3cec8acc64d0da97
nikodev <martin.panebianco@smandes.gob.ar>
2023-11-16 08:39:36 -0300

    update pk

--- vite.config.ts
+++ vite.config.ts
@@ -38,15 +38,15 @@ export default defineConfig({
   //   },
   // },
 
-  // preview: {
-  //   cors: true,
-  //   headers: {
-  //     "Access-Control-Allow-Origin": "*",
-  //     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-  //     "Access-Control-Allow-Headers":
-  //       "X-Requested-With, content-type, Authorization",
-  //   },
-  // },
+  preview: {
+    cors: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
+  },
 
   build: {
     modulePreload: false,
