c678a4768f4f75df0eee0480f85713d27c4a5007
nikodev <martin.panebianco@smandes.gob.ar>
2023-12-01 12:45:07 -0300

    Limpieza de archivos y librerias

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      filename: "mfe-parcel-entry.js",
-      name: "mfe-parcel",
-      exposes: {
-        "./ParcelTest": "./src/pages/ParcelTest",
-      },
-      shared: ["react", "react-dom"],
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
 
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
-  base: "/",
+  return {
+    plugins: [
+      react(),
+      federation({
+        filename: "mfe-parcel-entry.js",
+        name: "mfe-parcel",
+        exposes: {
+          "./ParcelTest": "./src/pages/ParcelTest",
+        },
+        shared: ["react", "react-dom"],
+      }),
+    ],
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+
+    base: process.env.VITE_BASENAME,
+  };
 });
